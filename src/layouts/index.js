a2bd5b63be6a202096c6a47833a7365f2e7c32ab
Ilya Ilin <ilin.i.1337@gmail.com>
2021-09-03 17:06:26 +0300

    feature/STYLES-Add-styles
    - Add bootstrap package
    - Add reactstrap package
    - Add bootstrap variables
    - Add base, common and helpers style sheets

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,11 @@
 // outsource libraries
 import React, { memo } from 'react';
+import { Container } from 'reactstrap';
 
 const Layouts = () => {
-  return <h1>Layouts</h1>;
+  return <Container fluid>
+    <h1>Layouts</h1>
+  </Container>;
 };
 
 export default memo(Layouts);
