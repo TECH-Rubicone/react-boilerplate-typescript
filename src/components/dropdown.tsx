f082a1ff365c0c40b8c5b19c73aa20a02a61a195
Ivan Chaban <ichabanivan@gmail.com>
2021-10-01 16:53:25 +0300

    feature/DROPDOWN-Implement-dropdown
    - Implement dropdown select

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,70 @@
+// outsource dependencies
+import React, { memo, useCallback, useState, useMemo, MouseEvent } from 'react';
+import { Button, MenuItem, ListItemIcon, ListItemText, Menu } from '@mui/material';
+import { SvgIconComponent, KeyboardArrowDown as KeyboardArrowDownIcon } from '@mui/icons-material';
+
+export interface DropdownOption {
+  key: string | number,
+  name: string
+  Icon: SvgIconComponent
+  handleClick: () => void
+}
+
+interface DropdownProps {
+  title: string
+  disabled?: boolean
+  options: Array<DropdownOption>
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ title, disabled, options }) => {
+  const [ref, setRef] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(ref);
+  const handleClick = useCallback((event: MouseEvent<HTMLElement>) => setRef(event.currentTarget), []);
+  const handleClose = useCallback(() => setRef(null), []);
+
+  const items = useMemo(() => options.map(({ handleClick, ...item }) => ({
+    ...item,
+    handleClick: () => {
+      handleClick();
+      handleClose();
+    }
+  })), [handleClose, options]);
+
+  return <>
+    <Button
+      disableElevation
+      disabled={disabled}
+      variant="contained"
+      onClick={handleClick}
+      endIcon={<KeyboardArrowDownIcon />}
+    >
+      { title }
+    </Button>
+    <Menu
+      open={isOpen}
+      anchorEl={ref}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+    >
+      { items.map(({ key, name, handleClick, Icon }) => <MenuItem
+        key={key}
+        onClick={handleClick}
+      >
+        <ListItemIcon>
+          <Icon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText>
+          { name }
+        </ListItemText>
+      </MenuItem>) }
+    </Menu>
+  </>;
+};
+
+Dropdown.defaultProps = {
+  disabled: false,
+  options: [],
+};
+
+export default memo(Dropdown);
