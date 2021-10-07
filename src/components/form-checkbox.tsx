e4ed7a1f13e89802f3ae91284d6172790b32f485
marii <mariachernovolos@gmail.com>
2021-10-07 14:21:38 +0300

    feature/MATERIAL-UI-Update-sign-in-styles-using-material-ui - add eslint check;

--- src/components/form-checkbox.tsx
+++ src/components/form-checkbox.tsx
@@ -3,7 +3,6 @@ import { useField } from 'formik';
 import React, { memo } from 'react';
 import { Checkbox, CheckboxProps, FormControlLabel, FormHelperText } from '@mui/material';
 
-
 interface FCheckboxProps extends CheckboxProps {
   name: string
   label?: string
@@ -13,7 +12,12 @@ const FCheckbox: React.FC<FCheckboxProps> = ({ label, name, ...attr }) => {
   const [field, meta] = useField({ name });
   return <>
     <FormControlLabel control={<Checkbox {...attr} {...field} onChange={field.onChange}/>} label={label}/>
-    { meta.touched && meta.error && <FormHelperText sx={{ color: 'error.main', m: 0, pb: 2 }} color="error">{meta.error}</FormHelperText>}
+    { meta.touched && meta.error && <FormHelperText
+      color="error"
+      sx={{ color: 'error.main', m: 0, pb: 2 }}
+    >
+      {meta.error}
+    </FormHelperText> }
   </>;
 };
 
