b574c9b6dd7c3f5bfd4229531a9cca26a9c83a00
Serhii Bohdan <serejabgdn@gmail.com>
2021-10-08 17:25:08 +0300

    feature/COMPONENTS-Implement-select - Add loading and loadingText properties

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -11,12 +11,14 @@ import { AnyObject } from 'interfaces/common';
 
 export interface SelectProps {
   name: string
+  loading?: boolean
   disabled?: boolean
   required?: boolean
   fullWidth?: boolean
   skipTouch?: boolean
   label: React.ReactNode
   size?: 'small' | 'medium'
+  loadingText?: React.ReactNode
   getOptionLabel?: (option: AnyObject) => string
   prepareValue: (value: AnyObject) => AnyObject | string | number | null
 }
@@ -27,7 +29,7 @@ interface FSelectProps extends SelectProps {
 
 const FSelect: React.FC<FSelectProps> = props => {
   const {
-    name, getOptionLabel, prepareValue, skipTouch,
+    name, getOptionLabel, prepareValue, skipTouch, loading, loadingText,
     disabled, fullWidth, required, size, options, label
   } = props;
 
@@ -39,8 +41,10 @@ const FSelect: React.FC<FSelectProps> = props => {
   return <FormControl fullWidth={fullWidth} color={validationStyles(valid, invalid)}>
     <Autocomplete
       getOptionLabel={getOptionLabel}
+      loadingText={loadingText}
       id={`select-${name}`}
       disabled={disabled}
+      loading={loading}
       options={options}
       size={size}
       {...field}
